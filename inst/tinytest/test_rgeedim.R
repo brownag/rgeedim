20b62ac014d1b90567b7c1ae705d2906f31ab863
Andrew Brown <andrew.g.brown@usda.gov>
2022-10-04 19:57:24 -0700

    Skip tests if modules unavailable

--- inst/tinytest/test_rgeedim.R
+++ inst/tinytest/test_rgeedim.R
@@ -1,17 +1,39 @@
-# bbox test
-expect_equal(
-  gd_bbox(
-    xmin = 5.744140,
-    xmax = 6.528252,
-    ymin = 49.44781,
-    ymax = 50.18162
-  ),
-  list(type = "Polygon", coordinates = list(list(
-    c(5.74414, 49.44781),
-    c(6.528252, 49.44781),
-    c(6.528252, 50.18162),
-    c(5.74414, 50.18162),
-    c(5.74414, 49.44781)
-  )))
-)
+## we are assuming gd_authenticate() has been called or otherwise set up
+# gd_authenticate(...)
 
+# init modules
+expect_null(gd_initialize())
+
+gd <- geedim()
+ee <- earthengine()
+
+# custom skip function
+.modules_available <- function() {
+  sapply(c(gd, ee), inherits, "python.builtin.module")
+}
+if (all(.modules_available())) {
+
+  # modules are loaded
+  expect_equal(.modules_available(), c(TRUE, TRUE))
+
+  # bbox test
+  expect_equal(
+    gd_bbox(
+      xmin = 5.744140,
+      xmax = 6.528252,
+      ymin = 49.44781,
+      ymax = 50.18162
+    ),
+    list(type = "Polygon", coordinates = list(list(
+      c(5.74414, 49.44781),
+      c(6.528252, 49.44781),
+      c(6.528252, 50.18162),
+      c(5.74414, 50.18162),
+      c(5.74414, 49.44781)
+    )))
+  )
+
+  expect_true(is.character(gd_version()))
+  expect_true(is.character(gd_ee_version()))
+
+}
