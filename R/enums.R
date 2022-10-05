c7f78786b8b6abd448a608ff06a0d9a4ec725950
Andrew Brown <andrew.g.brown@usda.gov>
2022-10-04 19:56:52 -0700

    Add enum methods for #1

---
+++ R/enums.R
@@ -0,0 +1,42 @@
+#' `geedim` Enums
+#'
+#' @param enum Enum name, one or more of: `"CloudMaskMethod"`, `"CompositeMethod"`, `"ResamplingMethod"`
+#'
+#' @return `gd_enum_names()`: character vector containing names of Enums
+#' @export
+#' @rdname enum
+gd_enum_names <- function() {
+  n <- names(gd$enums)
+  n[which(n != "Enum")]
+}
+
+#' @return `gd_enum_elements()`: element values of an Enum
+#' @export
+#' @rdname enum
+gd_enum_elements <- function(enum = gd_enum_names()) {
+  enum <- match.arg(enum, gd_enum_names(), several.ok = TRUE)
+  res <- lapply(enum, \(x) names(gd$enums[[x]]))
+  names(res) <- enum
+  res
+}
+
+#' @return `gd_resampling_methods()`: character vector of resampling methods (Enum `"ResamplingMethod"`)
+#' @export
+#' @rdname enum
+gd_resampling_methods <- function() {
+  gd_enum_elements("ResamplingMethod")[[1]]
+}
+
+#' @return `gd_cloud_mask_methods()`: character vector of cloud mask methods (Enum `"CloudMaskMethod"`)
+#' @export
+#' @rdname enum
+gd_cloud_mask_methods <- function() {
+  gd_enum_elements("CloudMaskMethod")[[1]]
+}
+
+#' @return `gd_composite_methods()`: character vector of composite methods (Enum `"CompositeMethod"`)
+#' @export
+#' @rdname enum
+gd_composite_methods <- function() {
+  gd_enum_elements("CompositeMethod")[[1]]
+}
