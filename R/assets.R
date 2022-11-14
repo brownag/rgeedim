062e9d3b4fc560130d86bbe0b618ae265890f8e9
Andrew Brown <andrew.g.brown@usda.gov>
2022-11-13 16:25:10 -0800

    docs

--- R/assets.R
+++ R/assets.R
@@ -1,39 +1,44 @@
 #' Get, Update, or Delete an Earth Engine Asset by ID
 #'
 #' @param x Asset ID name
-#' @param asset Used only for `gd_update_asset()`, a list representing elements of `x` to replace. Only the `"start_time"`, `"end_time"`, and `"properties"` fields can be updated. If a value is named in "updateFields", but is unset in `asset`, then that value will be deleted from the asset.
-#' @param updateFields Used only for `gd_update_asset()`. A character vector of field names to update. This may contain: "start_time" or "end_time" to update the corresponding timestamp, "properties.PROPERTY_NAME" to update a given property, or "properties" to update all properties. If the list is empty (default), all properties and both timestamps will be updated.
-#' @return This function is called for its side-effects (updates or deletes the specified asset)
+#' @param asset Used only for `gd_update_asset()`: a named list, with names representing elements of `x` to replace. Only `"start_time"`, `"end_time"`, and `"properties"` fields can be updated.
+#' @param update Used only for `gd_update_asset()`: A character vector of field names to update. Default: `"start_time"`, and `"end_time"` to update timestamps; and `"properties"` to update all properties.
+#' @param silent Silence errors? Default: `FALSE`
+#' @return `try-error` on error. `gd_get_asset()`: a named list containing information and properties of an Earth Engine asset
 #' @export
 #'
 #' @rdname existing-assets
 #' @examples
 #' \dontrun{
+#' # get asset from project by ID
 #' a <- gd_get_asset("projects/your-project-name/assets/YOUR_ASSET_ID")
 #' }
-gd_get_asset <- function(x) {
-  gd$utils$ee$data$getAsset(x)
+gd_get_asset <- function(x, silent = FALSE) {
+  try(gd$utils$ee$data$getAsset(x), silent = silent)
 }
 
 #' @rdname existing-assets
+#' @return `gd_update_asset()`: This function is called for side-effects (updates the specified asset fields)
 #' @examples
 #' \dontrun{
+#' # change description in `"properties"`
 #' a$properties$description <- "foo"
+#'
+#' # update asset
 #' gd_update_asset("projects/your-project-name/assets/YOUR_ASSET_ID", a, "properties")
 #' }
-gd_update_asset <- function(x, asset, updateFields = character(0)) {
-  if (length(updateFields) == 0) {
-    updateFields <- c("start_time", "end_time", "properties")
-  }
-  gd$utils$ee$data$updateAsset(x, asset, updateFields)
+gd_update_asset <- function(x, asset, update = c("start_time", "end_time", "properties"), silent = FALSE) {
+  try(gd$utils$ee$data$updateAsset(x, asset, update), silent = silent)
 }
 
 #' @rdname existing-assets
+#' @return `gd_delete_asset()`: This function is called for side-effects (deletes the specified asset)
 #' @export
 #' @examples
 #' \dontrun{
+#' # remove an asset from project
 #' gd_delete_asset("projects/your-project-name/assets/YOUR_ASSET_ID")
 #' }
-gd_delete_asset <- function(x) {
-  gd$utils$ee$data$deleteAsset(x)
+gd_delete_asset <- function(x, silent = FALSE) {
+  try(gd$utils$ee$data$deleteAsset(x), silent = silent)
 }
